cb6d5eb4f3adf64d4cf73610ed0536624c1a39d4
kjw2948 <kjw970313@cau.ac.kr>
2024-01-12 23:43:19 +0900

    java_class study

--- class1/ClassStart3.java
+++ class1/ClassStart3.java
@@ -1,4 +1,24 @@
 package class1;
 
 public class ClassStart3 {
+    public static void main(String[] args) {
+        Student student1 = new Student(); //x001
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        Student student2 = new Student(); //x002
+        student2.name = "학생2";
+        student2.age = 16;
+        student2.grade = 80;
+
+        Student[] students = new Student[2];
+        students[0] = student1;
+        students[1] = student2;
+
+        System.out.println("학생1의 정보 :"+ students[0].grade + students[0].age + students[0].name);
+
+
+    }
+
 }

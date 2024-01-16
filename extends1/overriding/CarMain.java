2dbb48d469ad99e282d5d5b266ab5e86abdd2388
kjw2948 <kjw970313@cau.ac.kr>
2024-01-16 15:09:58 +0900

    java_class study extends accessmodifier

---
+++ extends1/overriding/CarMain.java
@@ -0,0 +1,20 @@
+package extends1.overriding;
+
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElcectricCar electric = new ElcectricCar();
+        electric.move();
+        electric.charge();
+        electric.openDoor();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.fillHyrogen();
+        hydrogenCar.openDoor();
+
+    }
+}

90c3b2815f422fa40c843d452764c3f89331416c
kjw2948 <kjw970313@cau.ac.kr>
2024-01-21 16:11:37 +0900

    java_class study interface

---
+++ poly/ex5/InterfaceMain.java
@@ -0,0 +1,14 @@
+package poly.ex5;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Cow cow = new Cow();
+
+        cat.sound();
+        dog.sound();
+        cow.sound();
+
+    }
+}
